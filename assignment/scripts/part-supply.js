978ba27cd918b25a400a6ea8c2da54d203fac89e
Mark Terry <mark.terry93@gmail.com>
2021-12-27 19:10:08 -0600

    Stretch Goals

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -92,7 +92,7 @@ console.log('9. Filling boxes with a "while" loop');
 //  parts-=7;
 //}
 // This file wont open up on my computer anymore, I originally forgot to do parts-=7;
-// and accidentally had it going in a infinite loop
+// and accidentally had it going in a infinite loop i think???
 
 
 
